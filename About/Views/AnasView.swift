d474f3aff58f5ec58409934a76e68dd772563e67
Anas Muhsin <anashanoon2006@gmail.com>
2023-08-10 17:02:58 +0400

    Anas view commit

--- About/Views/AnasView.swift
+++ About/Views/AnasView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct AnasView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(colors: [.purple, .white, .red], startPoint: .topTrailing, endPoint: .bottomLeading);
+            
+            HStack(spacing: 50) {
+                Image("Anas")
+                    .resizable()
+                    .scaledToFit()
+                    .mask(Circle())
+                    .frame(width: 200)
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Link(destination: URL(string: "https://www.instagram.com/anas_alhanoon06/")!, label: {
+                        Text("Anas Al Hanoon")
+                            .foregroundColor(.black)
+                            .font(.system(size: 40))
+                            .bold()
+                    });
+                    
+                    Text("I am a student in Al Durrah International School")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                    
+                    HStack {
+                        Link(destination: URL(string: "https://www.nba.com/playoffs/2023")!) {
+                            Image(systemName: "basketball")
+                                .font(.system(size: 20))
+                                .foregroundColor(.black)
+                        };
+                        
+                        Link(destination: URL(string: "https://goo.gl/maps/UnWqR2YiDtwsim5b6")!, label: {
+                            Image(systemName: "location")
+                                .font(.system(size: 20))
+                                .foregroundColor(.black)
+                                
+                        });
+                        
+                        Link(destination: URL(string: "https://open.spotify.com/track/7nZwBbH8F8FqArrVIe8Zl4?si=7048ade0968e4e5e")!, label: {
+                            Image("Spotify_logo_without_text.svg")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25)
+                            
+                        });
+                    };
+                };
+            };
+        };
     }
 }
 
